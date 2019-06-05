dc6899a811012681cfc4ba1af36fe45f9007575f
Henry Ohanga <ohanga.henry@gmail.com>
2019-06-05 14:38:26 +0300

    update html for my pics tab

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
+import { PhotoService } from '../services/photo.service';
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
-  constructor() {}
-
+  constructor(public photoService: PhotoService) {}
 }
